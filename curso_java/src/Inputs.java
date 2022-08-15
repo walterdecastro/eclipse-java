06a3c8dfa56f006a7fdf5570228b8f92683c4c66
walterdecastro <walterdc1995@gmail.com>
2022-08-15 20:45:16 -0300

    curso java first lessons in src add

---
+++ curso_java/src/Inputs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Inputs {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String x;
+		x = sc.nextLine();
+		System.out.println("Você digitou: " + x);
+		
+		sc.close();
+
+	}
+
+}
