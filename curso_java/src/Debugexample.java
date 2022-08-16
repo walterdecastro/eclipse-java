de6ccca31670002e7a60e33cf689c2df8d003e16
walterdecastro <walterdc1995@gmail.com>
2022-08-16 00:27:41 -0300

    update

--- curso_java/src/Debugexample.java
+++ curso_java/src/Debugexample.java
@@ -12,6 +12,7 @@ public class Debugexample {
 		double largura = sc.nextDouble();
 		double comprimento = sc.nextDouble();
 		double metroQuadrado = sc.nextDouble();
+		
 		double area = largura * comprimento;
 		double preco = area * metroQuadrado;
 		
